5db8be3ecb423c7a1800a3bfbd92e71437692370
Ivan Bowen <itbowen@kabarak.ac.ke>
2023-05-18 09:53:04 +0300

    add role property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `
+  // <div>
+  //   <h1>This is an inline template</h1>
+  //   <p>This a template rendered in the UI</p>
+  // </div>`,
   styleUrls: ['./app.component.scss']
+  // styles: [
+  //   `h1 {color: red;}`
+  // ]
+
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
+
+  role = 'Admin'
 }
