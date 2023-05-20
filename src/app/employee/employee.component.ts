a644ec8538498ae128ad78cb31337d1c1dc7102a
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2023-05-20 10:10:06 +0300

    self anf skipself

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Self, SkipSelf, Host } from '@angular/core';
+import { RoomsService } from '../rooms/services/rooms.service';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.scss']
+  styleUrls: ['./employee.component.scss'],
+  // you want a different instance for this
+  providers: [RoomsService]
 })
 export class EmployeeComponent implements OnInit {
 
   employee: string = 'Mitchell';
+
+  constructor(@Host() private roomsService: RoomsService) {  }
 
   ngOnInit(): void {
     
