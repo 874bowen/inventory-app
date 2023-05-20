d3c104f067fe90d61607e12c7891528f2174f838
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2023-05-20 10:09:29 +0300

    using host class based

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit, AfterContentInit, ViewChild, AfterViewInit, ContentChild } from '@angular/core';
+import { Component, OnInit, AfterContentInit, ViewChild, AfterViewInit, ContentChild, Host } from '@angular/core';
 import { EmployeeComponent } from '../employee/employee.component';
+import { RoomsService } from '../rooms/services/rooms.service';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
-  styleUrls: ['./container.component.scss']
+  styleUrls: ['./container.component.scss'],
+  providers: [RoomsService]
 })
 export class ContainerComponent implements OnInit, AfterContentInit, AfterViewInit {
 
-  constructor() { }
+  constructor(@Host() private roomService: RoomsService) { }
 
   @ContentChild(EmployeeComponent) employeeComponent!: EmployeeComponent;
 
